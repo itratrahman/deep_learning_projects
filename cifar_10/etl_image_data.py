be749c4b9b2d70ead777bdbacb299b5c49dc1e16
Itrat Rahman <rahmanitrat@outlook.com>
2020-10-28 11:28:49 +0600

    created script of autoaugment

---
+++ cifar_10/etl_image_data.py
@@ -0,0 +1,15 @@
+import os
+import time
+import random
+import math
+import json
+import numpy as np
+from PIL import Image, ImageEnhance, ImageOps
+import cv2
+
+if __name__ = "__main__":
+
+    radom_seed = 0
+    random.seed(radom_seed)
+    numpy_seed = 0
+    np.random.seed(numpy_seed)
